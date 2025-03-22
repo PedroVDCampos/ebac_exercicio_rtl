c442fb7480730a851851d9e15d8ce1401351d8af
Pedro Campos <pedrovithorcampos19@gmail.com>
2025-03-22 12:09:15 -0300

    teste comentarios

---
+++ src/components/PostComments/PostComments.test.js
@@ -0,0 +1,16 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Post from "../components/Post";
+
+it("should add two comments to the list", () => {
+  render(<Post />);
+  const textarea = screen.getByRole("textbox");
+  const button = screen.getByRole("button", { name: /comentar/i });
+  fireEvent.change(textarea, { target: { value: "Primeiro comentário" } });
+  fireEvent.click(button);
+  fireEvent.change(textarea, { target: { value: "Segundo comentário" } });
+  fireEvent.click(button);
+  const comments = screen.getAllByRole("listitem");
+  expect(comments).toHaveLength(2);
+  expect(comments[0]).toHaveTextContent("Primeiro comentário");
+  expect(comments[1]).toHaveTextContent("Segundo comentário");
+});
